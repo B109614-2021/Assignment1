f2ee60f2df8b7451bf737de9ee57806bae42d3f5
B109614-2021 <s1734289@ed.ac.uk>
2021-10-18 14:04:52 +0100

    set fastq up to run for all samples, plan for extracting summary info

--- pipeline.sh
+++ pipeline.sh
@@ -10,8 +10,7 @@ read details
 # create a directory to hold temporary files in
 
 mkdir temp
-# mkdir fastqc_output 
-unset count
+# mkdir fastqc_output
 
 # use 100k.fqfiles to select sample files
 
@@ -21,12 +20,15 @@ do
 	do  	
 	if [[ $name == $End1 || $name == $End2 ]]; then
 		FilePath="$samples$name"	  	
-		echo $FilePath 
+		echo $FilePath
+		fastqc -o temp -f fastq --extract $FilePath 
 		fi
 	# filter for samples to be processed
-	# fastcq 
+	# fastcq
 	# save results in a tmp_file
-	# basic statistics can be found in the produced zip file in summary.txt and fastqc_data.txt  
+	# basic statistics can be found in the produced zip file in summary.txt and fastqc_data.txt
+	# --extract unzips output file, want to extract the relevant info, save in new document then delete unzipped files
+	# need correct field of file name to do this  
 	done 
 done < $details 
 
@@ -43,19 +45,19 @@ done < $details
 # Hisat2 -q for fastq (needs to be unzipped) --no-spliced-alignment to assume there are no splice sites, --qc-filter  remove bad reads 
 
 
- hisat2 [options]* -x <ht2-idx> {-1 <m1> -2 <m2> | -U <r>} [-S <sam>]
-
-  <ht2-idx>  Index filename prefix (minus trailing .X.ht2).
-  <m1>       Files with #1 mates, paired with files in <m2>.
-             Could be gzip'ed (extension: .gz) or bzip2'ed (extension: .bz2).
-  <m2>       Files with #2 mates, paired with files in <m1>.
-             Could be gzip'ed (extension: .gz) or bzip2'ed (extension: .bz2).
-  <r>        Files with unpaired reads.
-             Could be gzip'ed (extension: .gz) or bzip2'ed (extension: .bz2).
-  <sam>      File for SAM output (default: stdout)
-
-  <m1>, <m2>, <r> can be comma-separated lists (no whitespace) and can be
-  specified many times.  E.g. '-U file1.fq,file2.fq -U file3.fq'.
+# hisat2 [options]* -x <ht2-idx> {-1 <m1> -2 <m2> | -U <r>} [-S <sam>]
+#
+ # <ht2-idx>  Index filename prefix (minus trailing .X.ht2).
+  #<m1>       Files with #1 mates, paired with files in <m2>.
+ #            Could be gzip'ed (extension: .gz) or bzip2'ed (extension: .bz2).
+ # <m2>       Files with #2 mates, paired with files in <m1>.
+#             Could be gzip'ed (extension: .gz) or bzip2'ed (extension: .bz2).
+#  <r>        Files with unpaired reads.
+#             Could be gzip'ed (extension: .gz) or bzip2'ed (extension: .bz2).
+#  <sam>      File for SAM output (default: stdout)
+#
+#  <m1>, <m2>, <r> can be comma-separated lists (no whitespace) and can be
+#  specified many times.  E.g. '-U file1.fq,file2.fq -U file3.fq'.
 
 ### convert to bam file
 
