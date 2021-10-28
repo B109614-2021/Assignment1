626902d65566a3421e1cdcf0366f950f82192957
B109614-2021 <s1734289@ed.ac.uk>
2021-10-28 10:25:09 +0100

    make_folders.sh only copies files if they exist

--- make_folders.sh
+++ make_folders.sh
@@ -3,9 +3,12 @@
 # script that takes the input guide file, and sets up the desired folder structure.
 # want a folder for each sample, containing a folder for control and sample, containing a folder for each time point
 
+
+sample_folder=$1
+details=$2
+
 sample_names=$(awk '{FS="\t"; {print $2;}}' /localdisk/data/BPSM/AY21/fastq/100k.fqfiles | uniq | grep -v 'Sample')
 times=$(awk '{FS="\t"; {print $4;}}' /localdisk/data/BPSM/AY21/fastq/100k.fqfiles | sort | uniq | grep -v 'Time')
-sample_folder=/localdisk/data/BPSM/AY21/fastq
 
 rm -r temp
 
@@ -29,31 +32,33 @@ done
 # for each unique sample id, copy file into relevant folder
 # get unique sample IDs
 
-# sample_IDs=$(awk '{FS="\t"; {print $1;}}' /localdisk/data/BPSM/AY21/fastq/100k.fqfiles)
+# creat a variable listing paths to of where to copy from and to for end 1 and end 2 of samples 
 
 cp_end1=$(awk '{FS="\t"; {print "/" $6 " temp/" $2 "/" $5 "/" $4 "\n";}}' /localdisk/data/BPSM/AY21/fastq/100k.fqfiles)
 cp_end2=$(awk '{FS="\t"; {print "/" $7 " temp/" $2 "/" $5 "/" $4 "\n";}}' /localdisk/data/BPSM/AY21/fastq/100k.fqfiles)
 
 IFS=$'\n'
 
+# copy End1 of present samples into the relevant folder, testing that the files exist
+
 for file_path in $cp_end1
 do
-echo $file_path
-echo "$sample_folder$file_path" | xargs cp 
-
+file=$(echo $file_path | awk '{FS=" "; {print $1;}}')
+if test -f "$sample_folder$file";
+	then
+	echo "$sample_folder$file_path" | xargs cp 
+	fi
 done
+
+# copy End2 of present samples into the relevant folder, testing that the files exist
 
 for file_path in $cp_end2
 do
-echo $file_path
-echo "$sample_folder$file_path" | xargs cp
-
+file=$(echo $file_path | awk '{FS=" "; {print $1;}}')
+if test -f "$sample_folder$file";
+	then
+	echo "$sample_folder$file_path" | xargs cp
+	fi
 done
 
-# cp $sample_folder,$cp_end1
-#	if  [ "$treatment" = "Uninduced" ]
-#	then
-#	cp $sample_folder/$end_1_file temp/$sample_type/uninduced/$file_time
-#	cp $sample_folder/$end_2_file temp/$sample_type/uninduced/$file_time 
-#	fi
 
