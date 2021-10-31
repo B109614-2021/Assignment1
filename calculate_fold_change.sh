01e728968e5fbab339b31a18de9a2b10de15d9d8
B109614-2021 <s1734289@ed.ac.uk>
2021-10-31 21:12:16 +0000

    fold change is calcuated, but sorting is a problem, think there is an issue with tabs

--- calculate_fold_change.sh
+++ calculate_fold_change.sh
@@ -4,7 +4,16 @@
 # Then divide by time point 0
 # if heading sample name matches headed, extract
 
+# running the script if the temp versions alread exist causes bugs, so remove
+
+rm temp/fold_change_*
+rm temp/growing_*
+
 unset count
+
+unique_samples=$(awk '{FS="\t"; {print $2;}}' /localdisk/data/BPSM/AY21/fastq/100k.fqfiles | uniq | grep -v 'Sample')
+
+headers=$(head -n 1 output/output.tsv)
 
 # make a fold change for each sample
 
@@ -41,17 +50,53 @@ for sample in $unique_samples
 do
 counter=1
 fold_changes=$(find temp -name "fold_change*$sample*")
-echo $fold_changes
 	for head in $(head -n 1 $fold_changes)
 	do
-	echo $head
+	# find column with specific header, rm and save as seperate file
 		if [[ "$head" == *"Uninduced_0"* ]]
 		then
-		echo "match" 
-		cut -f$counter $fold_changes > temp/"$sample"_time_0.tsv 
+		# get uninduced_0, get everything apart from the header, and add 1 to remove any zeros
+		cut -f$counter $fold_changes | tail -n +2 |while read i; do echo "scale = 2; $i + 1" | bc; done > temp/"$sample"_time_0.tsv 
+		cut -f$counter --complement $fold_changes > temp/"$sample"_no_time_0.tsv
 		fi
 	counter=$((counter+1))
 	done 
-# find column with specific header, rm and save as seperate file 
-
 done 
+
+# take the temp/"$sample"_no_time_0 and divide columns (bar 1 and 2) by temp/"$sample"_time_0.tsv
+
+
+unset counter_2
+
+for sample in $unique_samples
+do
+awk '{FS="\t"; OFS="\t"; {print $1, $2;}}' output/output.tsv > temp/fold_change_"$sample"_2.tsv
+counter_2=1
+echo $sample
+fold_changes_2=$(find temp -name "$sample"_no_time_0*)
+echo $fold_changes_2
+        for head in $(head -n 1 $fold_changes_2)
+	do
+		# make sure to not use columns with strings
+                if [[ "$head" == *"nduced"* ]]
+		then
+		# add a header to temp file 
+		echo $head > temp/fold_change_value_"$sample".tsv
+		# get the header index, removing header so only values are taken
+		cut -f$counter_2 temp/"$sample"_no_time_0.tsv | tail -n +2 > temp/division.tsv
+		# divide by value at time 0 
+		paste temp/"$sample"_time_0.tsv temp/division.tsv | awk '{print $2/$1;}' >> temp/fold_change_value_"$sample".tsv 
+		# save in output file
+		paste temp/fold_change_"$sample"_2.tsv temp/fold_change_value_"$sample".tsv > temp/growing_fold_change_"$sample"_2.tsv
+		cat temp/growing_fold_change_"$sample"_2.tsv > temp/fold_change_"$sample"_2.tsv
+		fi
+	counter_2=$((counter_2+1))
+        done
+head -n 1 temp/fold_change_"$sample"_2.tsv | echo > output/fold_change_"$sample".tsv
+
+# sort -n -k 4 
+cat temp/fold_change_"$sample"_2.tsv >> output/fold_change_"$sample".tsv
+
+done
+
+
